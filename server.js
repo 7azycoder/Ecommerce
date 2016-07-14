a115ed796be94fc17bb5736e2a709c3699353ae5
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-07-15 01:34:57 +0530

    morgan library for logging into terminal added

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express(); // object of express
+
+//Middleware
+app.use(morgan('dev')); //object of morgan
+
+app.get('/',function(req,res){
+  var name = "Lovepreet";
+  res.json("My name is " + name);
+});
+
+app.get('/catname',function(req,res){
+  res.json('batman');
+});
 
 //this function is for starting the server
 //3000 is the port no
