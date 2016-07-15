587bd17bf493af3e011069809cb07f341b6b0d1a
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-07-16 03:33:33 +0530

    added all products and single product viewing functionality

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,6 @@
 var router = require('express').Router();
+var User = require('../models/user');
+var Product = require('../models/product');
 
 router.get('/',function(req,res){
   res.render('main/home');
@@ -6,6 +8,28 @@ router.get('/',function(req,res){
 
 router.get('/about',function(req,res){
   res.render('main/about');
+});
+
+// '/:id' is used to specify that this id can vary in request . The function below is creating dynamic links
+router.get('/products/:id',function(req,res,next){
+  Product
+  .find({category:req.params.id})
+  .populate('category')
+  .exec(function(err,products){
+    if(err) return next(err);
+    res.render('main/category',{
+      products:products
+    });
+  });
+});
+
+router.get('/product/:id',function(req,res,next){
+  Product.findById({_id:req.params.id},function(err,product){
+    if(err) return next(err);
+    res.render('main/product',{
+      product:product
+    });
+  });
 });
 
 module.exports = router;
