c8361604aac059e922b9a5f5d95765841220ac13
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-07-16 23:22:20 +0530

    added payment system,spinnner and view history

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,8 @@ var router = require('express').Router();
 var User = require('../models/user');
 var Product = require('../models/product');
 var Cart = require('../models/cart');
-
+var stripe = require('stripe')('sk_test_JvxDXJ0cUTuzyqL2jonZ5xNK');
+var async = require('async');
 function paginate(req,res,next){
   var perPage = 9;
   var page = req.params.page;
@@ -152,5 +153,55 @@ router.get('/product/:id', function(req, res, next) {
     });
   });
 });
+
+router.post('/payment', function(req, res, next) {
+
+  var stripeToken = req.body.stripeToken;
+  var currentCharges = Math.round(req.body.stripeMoney * 100);
+  stripe.customers.create({
+    source: stripeToken,
+  }).then(function(customer) {
+    return stripe.charges.create({
+      amount: currentCharges,
+      currency: 'usd',
+      customer: customer.id
+    });
+  }).then(function(charge) {
+    async.waterfall([
+      function(callback) {
+        Cart.findOne({ owner: req.user._id }, function(err, cart) {
+          callback(err, cart);
+        });
+      },
+      function(cart, callback) {
+        User.findOne({ _id: req.user._id }, function(err, user) {
+          if (user) {
+            for (var i = 0; i < cart.items.length; i++) {
+              user.history.push({
+                item: cart.items[i].item,
+                paid: cart.items[i].price
+              });
+            }
+
+            user.save(function(err, user) {
+              if (err) return next(err);
+              callback(err, user);
+            });
+          }
+        });
+      },
+      function(user) {
+        Cart.update({ owner: user._id }, { $set: { items: [], total: 0 }}, function(err, updated) {
+          if (updated) {
+            res.redirect('/profile');
+          }
+        });
+      }
+    ]);
+  });
+
+
+});
+
 
 module.exports = router;
